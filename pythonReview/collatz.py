64ed32717a738c3e0eb689280f38539f69668595
seanp <seanpatmeade@gmail.com>
2020-06-27 22:29:36 +0100

    Added user input for starting number.

--- pythonReview/collatz.py
+++ pythonReview/collatz.py
@@ -3,7 +3,7 @@
 # 2. If the number is odd, triple it and add one.
 
 # starting integer
-n = 20
+n = int(input("Please enter a positive integer:"))
 
 # while number is not 1
 while n != 1:
@@ -12,7 +12,7 @@ while n != 1:
     # if number is even
     if n % 2 == 0:
         # divide number by 2
-        n = n / 2
+        n = n // 2
     # Otherwise (so the number is odd)
     else:
         # multiply by 3 and add 1

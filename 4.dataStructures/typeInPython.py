d9beeb349e6539b1daebfa799168e361145bfc2e
seanp <seanpatmeade@gmail.com>
2020-08-11 21:18:41 +0100

    Adding more notes to differnt lectures.

---
+++ 4.dataStructures/typeInPython.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+x = np.array([1, 2, 3, 4])
+
+print(x)
+print(type(x))
+
+i = 1
+
+print(type(i))
+
+j = 1.0
+
+print(type(j))
+
+s = "Sean"
+
+print(type(s))
+
+m = [1, 2, 3, 4]
+
+print(type(m))
+
+class y:
+    a = 1
+
+print(y)
+print(y.a)

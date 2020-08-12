1a61554ba0404acee5948cbe599e6662e3b6839a
seanp <seanpatmeade@gmail.com>
2020-08-12 23:45:58 +0100

    Adding notes on plotting.

---
+++ 5.plots/pyplot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
+
+plt.plot([1, 4, 9, 16])
+plt.show()
+
+# k. = black small dots b- = blue line
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+plt.show()
+
